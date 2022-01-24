bc5b30635b093e2b54e83727d998dace5c5ab060
Jeremy Magland <jeremy.magland@gmail.com>
2022-01-24 13:38:21 -0500

    No dependency on typing Literal

--- kachery_client/task_backend/taskfunction.py
+++ kachery_client/task_backend/taskfunction.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, List, Literal, Union
+from typing import Any, Callable, Dict, List, Union
 
 _global_registered_taskfunctions_by_function_id: Dict[str, Callable] = {}
 
@@ -11,7 +11,7 @@ def find_taskfunction(function_id: str) -> Union[Callable, None]:
 def all_taskfunction_ids() -> List[str]:
     return list(_global_registered_taskfunctions_by_function_id.keys())
 
-def taskfunction(function_id: str, *, type: Union[Literal['pure-calculation'], Literal['query'], Literal['action']]):
+def taskfunction(function_id: str, *, type: str):
     def wrap(f: Callable[..., Any]):
         setattr(f, '_task_function_type', type)
         setattr(f, '_task_function_id', function_id)
